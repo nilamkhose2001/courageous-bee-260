dfb588ec428ed2de954b79680b8b94289e41d413
Nikhil1368 <avihaansharma1308@gmail.com>
2022-07-23 16:42:00 +0530

    final touch

--- components/navport.js
+++ components/navport.js
@@ -6,7 +6,7 @@ function navbar()
     <div><a href="./search.html"><img src="https://cdn-icons-png.flaticon.com/128/7289/7289312.png" alt=""></a></div>
     <div><a href=""><img src="https://cdn-icons-png.flaticon.com/128/1216/1216811.png" alt=""></a></div>
     <div><a href="signup.html"><img src="https://cdn-icons-png.flaticon.com/128/6423/6423509.png" alt=""></a></div>
-    <div style = "display:flex; justify_content:space-between; width : 35px;"><a href="cart.html"><img src="https://cdn-icons-png.flaticon.com/128/7269/7269930.png" alt="" style = "width : 80%;"></a><span id = "count" style = "color: red;">0</span></div>
+    <div style = "display:flex; justify_content:space-between; width : 35px;"><a href="cart.html"><img src="https://cdn-icons-png.flaticon.com/128/7269/7269930.png" alt="" style = "width : 80%;"></a><span id = "count" style = "color: red; font-weight : 1000; font-size :14px;">0</span></div>
 `}
 
 export default navbar;
